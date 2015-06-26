53a6a93b0aeaec8c7a93b466f1bcf23e94639c0e
JardelAnton <jardelanton@hotmail.com>
2015-06-26 08:51:16 -0300

    Main

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
-
-
 import java.util.Scanner;
 import java.io.*;
 
 
 class Main {
-    public static void main(String args[])be {
-        
-        
-        
-        
-        
-        
+
+    public static void main(String args[]) throws Exception {
+		Numb comeco= new Numb();
+		Numb  fim= new Numb();
+
+
+		Scanner s= new Scanner(System.in);
+		comeco.inteiro=s.nextInt();
+		fim.inteiro=s.nextInt();
+
+
+
+
         
     }
 }
