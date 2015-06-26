5734d8928dd58e3b34063318c8ca40629638f24f
DaviRP <davi.pegoraro@hotmail.com>
2015-06-26 08:46:56 -0300

    eh_primo

--- Numb.java
+++ Numb.java
@@ -2,7 +2,42 @@ class Numb{
 
     public int inteiro;
     
-
     
-
+    public eh_inteiro(Numb Comeco,Numb Fim){
+        
+        int count;
+        int j;
+        
+        
+        for(Comeco.inteiro=0;Comeco.inteiro<fim.inteiro;Comeco.inteiro++){
+            
+            
+            for(j=0;j<Comeco.inteiro;j++){
+                
+                
+                if(j%2=0){
+                    
+                    count++;
+                    
+                }
+                
+                
+                
+                
+            }
+            
+            if(count==2){
+                
+                System.out.println(Comeco.inteiro);
+                
+            }
+                
+            count=0;
+            
+        }
+        
+    }
+    
+    
+    
 }
---
+++ palavras.java
@@ -0,0 +1,9 @@
+class palavras{
+    
+    string palavra;
+    
+    
+    public void 
+    
+    
+}
